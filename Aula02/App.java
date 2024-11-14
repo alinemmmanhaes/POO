bae20a6ea6594f8604e0451299f2df1ced98ffd5
alinemmmanhaes <aline.manhaes@edu.ufes.br>
2024-11-14 14:26:43 -0300

    antepassados

--- Aula02/App.java
+++ Aula02/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) {
+        //cria familia
         Pessoa aline = new Pessoa(null, null, "Aline");
         Pessoa tales = new Pessoa(null, null, "Tales");
         Pessoa caio = new Pessoa(tales, aline, "Caio Henrique");
@@ -7,23 +8,24 @@ public class App {
         Pessoa barbara = new Pessoa(caio, raony, "Bárbara");
         Pessoa marcela = new Pessoa(caio, raony, "Marcela");
         
+        //testa getnome
         System.out.println(caio.getNome() + ": mãe " + caio.getMae().getNome() + " e pai " + caio.getPai().getNome());
         System.out.println(raony.getNome() + ": mãe " + raony.getMae().getNome() + " e pai " + raony.getPai().getNome());
 
+        //testa funcao familia 
         marcela.familia();
 
+        //testa funcao de arvore genealogica
         System.out.println("\nÁrvore de " + barbara.getNome() + ":");
         barbara.geraArvore();
         System.out.println();
-
-        System.out.println("\nÁrvore de " + raony.getNome() + ":");
-        raony.geraArvore();
         System.out.println();
 
-        System.out.println("\nÁrvore de " + aline.getNome() + ":");
-        aline.geraArvore();
-        System.out.println();
-
+        //testa funcao de lista de antepassados com print
         System.out.println(barbara.getAntepassados());
+        System.out.println();
+
+        //testa print de pessoa
+        System.out.println(aline);
     }
 }
